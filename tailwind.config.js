f0508bc12031c8a7b05671401a359e3e5af667bc
mbarto202 <michael.d.barto@gmail.com>
2024-10-12 20:48:19 -0400

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}", // Include all HTML and JS files in the src directory
+    "./public/index.html", // Include your main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
