a309b21d2efe5056f6683d72ea163f906a7a4924
mbarto202 <michael.d.barto@gmail.com>
2024-10-17 22:33:54 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./public/index.html", // Include your main HTML file
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
